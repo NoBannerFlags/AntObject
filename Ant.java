d427faddefbd1c0b66cd4f8f097e7279a79e406d
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-03-06 14:09:34 -0700

    Update Ant.java
    -Improved documentation for the eat method.

--- Ant.java
+++ Ant.java
@@ -7,8 +7,12 @@ public interface Ant{
     4: Sugar
     5: Junk (Human trash)
     6: Inset bits
-    7: 
-    
+    7: Small insect
+    8: Meat
+    9: Other waste
+    10: Other liquid
+    11: Other scavenge
+    12: Other prey
     **/
     public String sense();
     public void bite(int bitepower, String target);
