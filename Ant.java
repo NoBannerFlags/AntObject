575076dc5e506fa23428b14b0ddc2272ba8caed5
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-03-06 14:07:28 -0700

    Update Ant.java
    -Updated the Ant.java interface & interface requirements.
    -Added documentation for the improved eat(); method.
    -Added void bite

--- Ant.java
+++ Ant.java
@@ -1,4 +1,15 @@
 public interface Ant{
-    public void eat();
+    public void eat(int foodType);
+    /*Food types:
+    1: Leaf
+    2: Fungus
+    3: Honey
+    4: Sugar
+    5: Junk (Human trash)
+    6: Inset bits
+    7: 
+    
+    **/
     public String sense();
+    public void bite(int bitepower, String target);
 }
