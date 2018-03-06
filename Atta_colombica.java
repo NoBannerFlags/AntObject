4ed3369a623357db71e9238ea14a743732cec11f
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-03-06 14:42:48 -0700

    Update Atta_colombica.java

--- Atta_colombica.java
+++ Atta_colombica.java
@@ -1,4 +1,7 @@
-//Atta colombica is the fungus farming ant.
-public Interface Atta_C{
-
+//Atta colombica is the fungus farming ant. This interface is implimented by any ant that is this species
+public interface Atta_C{
+  //Have the ant cut some plants in order to feed the fungi.
+  public void cutGreens();
+  //Have the ant clean the fungi
+  public void cleanFungus();
 }
