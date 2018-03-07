76a83d95484012c5d1855891ef1fdc25e8a48372
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-03-07 14:40:46 -0700

    Update workerAtta.java

--- workerAtta.java
+++ workerAtta.java
@@ -1,6 +1,14 @@
 import java.util.*;
 public class WorkerAtta{
-  private int antAge;
+  //amount of food the ant has. at 10 food, it becomes hungry, at 0 it dies.
+  private int foodLevel;
+  //the rate at which the ant's food reserves deteriorate, defaults to 1.
+  private int metabolism = 1;
+  //age of the ant in days, dies at 30, defaults to 0.
+  private int antAge = 0;
   private boolean hungerSatisfied;
   private boolean isGrown;
+  public void cutGreens(){
+    
+  }
 }
