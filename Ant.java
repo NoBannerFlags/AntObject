7e5a95b47424652f9bf5e135b733db68a9c177a7
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-03-13 10:59:57 -0600

    Update Ant.java

--- Ant.java
+++ Ant.java
@@ -1,18 +1,18 @@
 public interface Ant{
     public void eat(int foodType);
     /*Food types:
-    1: Leaf
-    2: Fungus
-    3: Honey
-    4: Sugar
-    5: Junk (Human trash)
-    6: Inset bits
-    7: Small insect
-    8: Meat
-    9: Other waste
-    10: Other liquid
-    11: Other scavenge
-    12: Other prey
+    *1: Leaf
+    *2: Fungus
+    *3: Honey
+    *4: Sugar
+    *5: Junk (Human trash)
+    *6: Inset bits
+    *7: Small insect
+    *8: Meat
+    *9: Other waste
+    *10: Other liquid
+    *11: Other scavenge
+    *12: Other prey
     **/
     public String sense();
     public void bite(int bitepower, String target);
