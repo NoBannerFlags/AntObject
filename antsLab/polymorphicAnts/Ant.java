a246292045af7fc260a09b8f2c093bfa9fb977b6
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2018-05-23 13:01:36 -0600

    Movement of Ant interface to a package

--- antsLab/polymorphicAnts/Ant.java
+++ antsLab/polymorphicAnts/Ant.java
@@ -1,9 +1,9 @@
-
+package polymorphicAnts;
 /**
  * All objects that are a "ant" implement this interface
  *
  * @author Malokai Persoff
- * @version 0.1
+ * @version 0.2
  * @since WIPE_1
  */
 public interface Ant
@@ -11,14 +11,14 @@ public interface Ant
     
     /**
      * Method eat  The ant eats <code>cal</code> Calories
-     *
+     * @since 0.1
      * @param cal  The number of calories in the current piece of food 
      */
     public void eat(int cal);
     
     /**
      * Method die  The ant dies, causes its value in the ArrayList to be removed
-     * 
+     * @since 0.1
      */
     public void die();
 }
