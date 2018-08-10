afdb7f53baa08593c5208dec7ed4b2c66be47aa9
NoBannerFlags <35818117+NoBannerFlags@users.noreply.github.com>
2018-08-10 10:03:43 -0600

    Minor fixes

---
+++ antsLab/polymorphicAnts/sim.java
@@ -0,0 +1,12 @@
+package polymorphicAnts;
+import java.util.*;
+
+
+public class sim{
+    private int cFood = 0; //Colony's food
+    
+    public static void main(String[] args){
+        
+    }
+}
+
